593de2265b96e257b0d43cfff11f92b2fa06090f
Jesús Antonio Rozo <80847856+Jesusdebug@users.noreply.github.com>
2025-05-14 16:57:11 -0500

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+        //// 
+        //// GET: /HelloWorld/Welcome/ 
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+    }
+}
